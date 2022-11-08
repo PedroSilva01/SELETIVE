fb3d5a4a075ee03bae6867c6b6ce51f42c0041f2
PedroSilva01 <pedrovictor94442500@gmail.com>
2022-11-08 18:04:30 -0300

    routing e get dos dados dos forms

--- empresa/views.py
+++ empresa/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
+from .models import Tecnologia
 
 # Create your views here.
 def nova_empresa(request):
-    return render(request, 'nova_empresa.html')
+    if request.method == "GET":
+        techs = Tecnologia.objects.all()
+        return render(request, 'nova_empresa.html', {'techs': techs})
+    elif request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        cidade = request.POST.get('cidade')
+        endereco = request.POST.get('endereco')
+        nicho = request.POST.get('nicho')
+        tecnologias = request.POST.getlist('tecnologias')
+        caracteristicas = request.POST.get('caracteristicas')
+        logo = request.FILES.get('logo')
+        return HttpResponse("To aqui")
